bb0ce4f6fed3d3c558fae87df070764971dce37a
Kurt Haeusler <kurt.haeusler@gmail.com>
2010-05-29 18:37:00 +0200

    Initial empty rails project for MealClub

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '7233199b34d652e6c33decc8ebec76d1675fc51919935815caf1bd4e80be076cbaa05f9dac9aa87e371a32a6ee07d51e469fcc57ea46d666b1af17a918f9b9b0'
