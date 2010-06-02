7dfb8e5cf89db632856bfe794f68a988ae440cb0
Kurt Haeusler <kurt.haeusler@gmail.com>
2010-06-03 01:24:10 +0800

    Can now add a cook to a meal. If a member exists with that name it will be used. Otherwise a new member will be added and assigned to the meal as a cook.

--- app/controllers/cooks_controller.rb
+++ app/controllers/cooks_controller.rb
@@ -1,2 +1,25 @@
 class CooksController < ApplicationController
+  def create
+    @meal = Meal.find(params[:meal_id])
+    @member = Member.first(:conditions => ["name = ?", params[:cook][:name]])
+    if not @member
+      @member = Member.new
+      @member.name = params[:cook][:name]
+      @member.save
+    end
+    
+    @cook = @meal.meal_cooks.create
+    @cook.meal = @meal
+    @cook.member = @member
+
+    respond_to do |format|
+      if @cook.save
+        format.html { redirect_to(@meal, :notice => 'Cook was successfully created.') }
+        format.xml  { render :xml => @meal, :status => :created, :location => @meal }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @meal.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
 end
